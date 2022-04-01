0c792416345ee6e5d8fc5e95590d36a1c5eb6a51
cacachandrika <49362819+cacachandrika@users.noreply.github.com>
2022-04-01 20:20:38 +0700

    Update prewitt.m

--- src/prewitt.m
+++ src/prewitt.m
@@ -1,7 +1,18 @@
-function edge_image = prewitt(I)
+function edgeIm = prewitt(I)
+    [M,N,D]=size(I);
     Px = [-1 0 1; -1 0 1; -1 0 1];
-    Py = [-1 -1 -1; 0 0 0; 1 1 1];
+    Py = [1 1 1; 0 0 0; -1 -1 -1];
     Jx = convn(double(I), double(Px), 'same');
     Jy = convn(double(I), double(Py), 'same');
-    edge_image = sqrt(Jx.^2 + Jy.^2);
+    edgeIm = sqrt(Jx.^2 + Jy.^2);
+    for i = 1:M
+        edgeIm(i,1)=0;
+        edgeIm(i,N)=0;
+    end
+    for j = 1:N
+        edgeIm(1,j)=0;
+        edgeIm(M,j)=0;
+    end
+    edgeIm = uint8(edgeIm);
+%     figure, imshow(edgeIm);
 end
