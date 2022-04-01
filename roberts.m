b4875fc1fc931cce41cfb19efd1b674a7b2c386d
cacachandrika <49362819+cacachandrika@users.noreply.github.com>
2022-04-01 12:08:34 +0700

    Update roberts.m

--- roberts.m
+++ roberts.m
@@ -3,6 +3,6 @@ function edgeIm = roberts(I)
     Ry = [0 1; -1 0];
     Jx = conv2(double(I), double(Rx), 'same');
     Jy = conv2(double(I), double(Ry), 'same');
-    Jedge = sqrt(Jx.^2 + Jy.^2);
-    figure, imshow(uint8(Jedge));
+    edgeIm = sqrt(Jx.^2 + Jy.^2);
+    figure, imshow(uint8(edgeIm));
 end
