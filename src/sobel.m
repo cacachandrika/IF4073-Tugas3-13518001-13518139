9aff978a2ccb8c5384bf1c95d229f1672b5bf023
cacachandrika <49362819+cacachandrika@users.noreply.github.com>
2022-04-01 20:20:56 +0700

    Update sobel.m

--- src/sobel.m
+++ src/sobel.m
@@ -1,7 +1,17 @@
-function edge_image = sobel(I)
+function edgeIm = sobel(I)
+    [M,N,D]=size(I);
     Sx = [-1 0 1; -2 0 2; -1 0 1];
     Sy = [1 2 1; 0 0 0; -1 -2 -1];
     Jx = convn(double(I), double(Sx), 'same');
     Jy = convn(double(I), double(Sy), 'same');
-    edge_image = sqrt(Jx.^2 + Jy.^2);
+    edgeIm = uint8(sqrt(Jx.^2 + Jy.^2));
+    for i = 1:M
+        edgeIm(i,1)=0;
+        edgeIm(i,N)=0;
+    end
+    for j = 1:N
+        edgeIm(1,j)=0;
+        edgeIm(M,j)=0;
+    end
+%     figure, imshow(uint8(edgeIm));
 end
