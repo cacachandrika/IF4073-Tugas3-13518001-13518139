36c9a1fcb438f2cdce017bdb98d7a4978374fd76
cacachandrika <49362819+cacachandrika@users.noreply.github.com>
2022-04-01 12:08:18 +0700

    Update prewitt.m

--- prewitt.m
+++ prewitt.m
@@ -3,6 +3,6 @@ function edgeIm = prewitt(I)
     Py = [-1 -1 -1; 0 0 0; 1 1 1];
     Jx = conv2(double(I), double(Px), 'same');
     Jy = conv2(double(I), double(Py), 'same');
-    Jedge = sqrt(Jx.^2 + Jy.^2);
-    figure, imshow(uint8(Jedge));
+    edgeIm = sqrt(Jx.^2 + Jy.^2);
+    figure, imshow(uint8(edgeIm));
 end
